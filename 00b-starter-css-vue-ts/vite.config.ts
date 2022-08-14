713ce7a9599aa3574e3f6c341f370f9533ddd997
Rémi Connesson <remiconnesson@gmail.com>
2022-08-14 22:29:03 +0200

    Install vite plugin checker

--- 00b-starter-css-vue-ts/vite.config.ts
+++ 00b-starter-css-vue-ts/vite.config.ts
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"', // for example, lint .ts & .tsx
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
